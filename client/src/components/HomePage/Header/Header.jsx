e545f559dc810a423f91f3efa71ea902b0c3df77
Ashley Pean <pean.ashley@gmail.com>
2020-11-27 06:07:19 -0500

    .

--- client/src/components/HomePage/Header/Header.jsx
+++ client/src/components/HomePage/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useHistory, Link } from 'react-router-dom'
-import HeaderLogo from '/Users/ashley/Library/Mobile Documents/com~apple~CloudDocs/Coding/Projects/cat-wiki/src/img/logo-white.svg'
+import HeaderLogo from '../../../img/logo-white.svg'
 import './Header.css'
 
 export default function Header(props) {
