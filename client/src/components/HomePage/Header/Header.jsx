1417b35fadac0e826ed1e1684385479675c04e08
Ashley Pean <pean.ashley@gmail.com>
2020-11-27 06:00:11 -0500

    .

--- client/src/components/HomePage/Header/Header.jsx
+++ client/src/components/HomePage/Header/Header.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { useHistory, Link } from 'react-router-dom'
-import HeaderLogo from '../../../img/logo-white.svg'
+import HeaderLogo from '/Users/ashley/Library/Mobile Documents/com~apple~CloudDocs/Coding/Projects/cat-wiki/src/img/logo-white.svg'
 import './Header.css'
 
 export default function Header(props) {
   const [tempNames, setTempNames] = useState(['', ''])
   const [constNames, setConstNames] = useState(['', ''])
-  // const [isOpen, setIsOpen] = useState(false)
+
   //Keep track of our browser history so we can redirect to search results
   const history = useHistory()
 
@@ -14,14 +14,6 @@ export default function Header(props) {
   setTempNames(props.names)
   setConstNames(props.names)
  }, [props])
-
-
-  const handleSelect = (e) => {
-    const selection = e.target.innerText
-    console.log(selection)
-    const searchString = `breeds/search/${selection}`
-    history.push(searchString)
-  } 
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -55,21 +47,24 @@ export default function Header(props) {
     }
   }
 
-  const handleFocus = () => {
+  const handleFocus = (e) => {
     //Store the input field, the div.names-list and ul.names-list elements
     const input = document.querySelector('input.search')
     const namesDiv  = document.querySelector('div.names-list')
     const namesList = document.querySelector('ul.names-list')
 
-    //If input list is active, show the div and ul, if not - hide them
-    if(document.activeElement === input) {
-      namesDiv.classList.remove('hidden')
-      namesList.classList.remove('hidden')
-      
-    } else {
-      namesDiv.classList.add('hidden')
-      namesList.classList.add('hidden')
-    }
+    //Set timeout needed so that page will redirect if link is clicked 
+    //Otherwise user clicking on breedname will not respond
+    setTimeout( () => {
+      if(document.activeElement === input ) {
+        namesDiv.classList.remove('hidden')
+        namesList.classList.remove('hidden')
+        
+      } else if(document.activeElement !== input ) {
+        namesDiv.classList.add('hidden')
+        namesList.classList.add('hidden')
+      }
+    }, 100)
     
   }
 
@@ -88,9 +83,11 @@ export default function Header(props) {
 
           <div className="names-list hidden">
             <ul className="names-list hidden">
-              {tempNames.map((name, index) => (
-                <li key={index} onClick={handleSelect}> {name} </li>  
-                ))}
+              {tempNames.map((name, index) => 
+                <Link to={`/breeds/search/${name}`}>
+                  <li key={index}> {name} </li>
+                </Link>
+              )}
             </ul>
           </div>
         </form>
